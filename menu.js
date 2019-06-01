fae91280acaf48cfbb0b1da083c99756a340c129
calvinschmidt4 <calvinschmidt4@users.noreply.github.com>
2019-06-01 10:47:42 +0200

    menu

--- menu.js
+++ menu.js
@@ -4,38 +4,62 @@
   if ( w <= targetWidth) {     
   //Add your javascript for screens wider than or equal to 768 here
   document.getElementById("togglemenu").onclick =
-  function toggle_visibility(hide) {
-         var e = document.getElementById("hide");
-         if(e.style.display == 'block')
-            e.style.display = 'none';
-         else
-            e.style.display = 'block';
-      }
+    function toggle_visibility() {
+      var m = document.getElementById("hide");
+      if(m.style.display == 'block')
+          m.style.display = 'none';
+      else
+          m.style.display = 'block';
 
+      var s = document.getElementById("hamburger");
+      if(s.style.display == 'none')
+          s.style.display = 'block';
+      else
+          s.style.display = 'none';  
+     
+      var d = document.getElementById("x");
+      if(d.style.display == 'block')
+          d.style.display = 'none';
+      else
+          d.style.display = 'block';
+    }
+  
   document.getElementById("toggle").onclick =
-  function toggle_visibility(hide) {
-         var e = document.getElementById("hide");
-         if(e.style.display == 'block')
-            e.style.display = 'none';
-         else
-            e.style.display = 'block';
-      }    
+    function toggle_visibility(hide) {
+      var l = document.getElementById("hide");
+      if(l.style.display == 'block')
+          l.style.display = 'none';
+      else
+          l.style.display = 'block';
+          
+      var s = document.getElementById("hamburger");
+      if(s.style.display == 'none')
+          s.style.display = 'block';
+      else
+          s.style.display = 'none';  
+     
+      var d = document.getElementById("x");
+      if(d.style.display == 'block')
+          d.style.display = 'none';
+      else
+          d.style.display = 'block';
+    }    
   }   
 
-var slideIndex = 1;
-showDivs(slideIndex);
+// var slideIndex = 1;
+// showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
+// function plusDivs(n) {
+//   showDivs(slideIndex += n);
+// }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
-}
+// function showDivs(n) {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   if (n > x.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = x.length}
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";  
+//   }
+//   x[slideIndex-1].style.display = "block";  
+// }
